eac2c9d73c018d5557ec77bb0587965f11ead7e7
afetism <aismayilova1215@gmail.com>
2024-04-05 20:27:35 +0400

    adding signin and signup for librarian

--- LibraryManagmentWeb/Controllers/HomeController.cs
+++ LibraryManagmentWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryManagmentWeb.Models;
+﻿using LibraryManagmentWeb.Database;
+using LibraryManagmentWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -12,27 +13,54 @@ namespace LibraryManagmentWeb.Controllers
 		{
 			_logger = logger;
 		}
-
+		[HttpGet]
 		public IActionResult SignIn()
 		{
 			return View();
 		}
-
+		[HttpGet]
 		public IActionResult Privacy()
 		{
 			return View();
 		}
-
+		[HttpGet]
 		public IActionResult SignUp()
 		{
 			
 			return View();
 		}
+		[HttpPost]
+		public IActionResult SignIn(Librarian librarian)
+		{
+			LibrarianBase _librarian = new ();
+			var status = _librarian.Librarians.FirstOrDefault(e => e.UserName == librarian.UserName &&
+			e.Password == librarian.Password);
+			if (status == null)
+			{
+				ViewBag.LoginStatus = 0;
+			}
+			else
+			{
+				return RedirectToAction("Welcome Again!", "Home", new { employee = status });
+			}
+			return View(librarian);
+		}
 
 
 
+		[HttpPost]
+		public IActionResult Register(Librarian librarian)
+		{
+			LibrarianBase _librarian = new();
 
+			if (ModelState.IsValid)
+			{
+				_librarian.Librarians.Add(librarian);
+				return RedirectToAction("SuccessPage", "Home", new { employee = librarian });
+			}
 
+			return View(librarian);
+		}
 
 
 
